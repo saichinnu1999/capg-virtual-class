56385a868e57680a969b20d0af94e13e0df27d9d
dell <dell@192.168.0.8>
2020-04-10 15:32:19 +0530

    m r

---
+++ capg-movie-rating/src/main/java/com/capg/movierating/model/MovieRating.java
@@ -0,0 +1,23 @@
+package com.capg.movierating.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class MovieRating {
+
+	@Id
+	private Long id;
+	private double rating;
+}
