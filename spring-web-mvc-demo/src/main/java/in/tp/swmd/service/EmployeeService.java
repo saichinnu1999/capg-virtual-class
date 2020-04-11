d9250aa4030338ae149f07b2539f000b69e4e136
dell <dell@192.168.0.8>
2020-04-11 14:55:23 +0530

    jpa

---
+++ spring-web-mvc-demo/src/main/java/in/tp/swmd/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package in.tp.swmd.service;
+
+import in.tp.swmd.model.Employee;
+
+public interface EmployeeService {
+	
+	void employeedetails(Employee employee);
+	
+}
---
+++ spring-web-mvc-demo/src/main/java/in/tp/swmd/service/LoanService.java
@@ -0,0 +1,8 @@
+package in.tp.swmd.service;
+
+import in.tp.swmd.model.Loan;
+
+public interface LoanService {
+
+void computeintrest(Loan loan) ;
+}
